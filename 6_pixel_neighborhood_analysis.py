a3a8cdb968e31eb34a8a9981e0b5081966958c76
shakib75bd <shakib.csebrur@gmail.com>
2025-09-15 20:59:52 +0600

    6,7

--- 6_pixel_neighborhood_analysis.py
+++ 6_pixel_neighborhood_analysis.py
@@ -1,11 +1,15 @@
-"""Task: Pixel Neighborhood Analysis"""
+#4-neighbours and 8-neighbours in image
 import cv2
-img = cv2.imread('image.jpg', cv2.IMREAD_GRAYSCALE)
-x, y = img.shape[0]//2, img.shape[1]//2
-val = img[x, y]
+img=cv2.imread('image.jpg', cv2.IMREAD_GRAYSCALE)
+
+x = img.shape[0]//2
+y = img.shape[1]//2
+
+val = img[x,y]
+
+#four neighbour
 four = [(x-1,y),(x+1,y),(x,y-1),(x,y+1)]
-eight = four + [(x-1,y-1),(x-1,y+1),(x+1,y-1),(x+1,y+1)]
-print(f"Pixel ({x},{y}) value: {val}")
-print("4-neighbors:", four)
-print("8-neighbors:", eight)
-# Neighborhoods are key for filtering and edge detection.
+eight = four + [(x-1,y-1), (x+1,y+1),(x+1,y-1),(x-1,y+1)]
+
+print('4 neighbours: ', four)
+print('8 neighbours: ', eight)
