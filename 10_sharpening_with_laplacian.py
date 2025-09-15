2bf5505b4840aecdd0a2f44d8223a689802e8fe9
shakib75bd <shakib.csebrur@gmail.com>
2025-09-16 00:06:21 +0600

    8,9,10 simplified

--- 10_sharpening_with_laplacian.py
+++ 10_sharpening_with_laplacian.py
@@ -1,11 +1,13 @@
-"""Task: Sharpening with Laplacian Filter"""
 import cv2
 import matplotlib.pyplot as plt
+
 img = cv2.imread('image.jpg', cv2.IMREAD_GRAYSCALE)
+
+# Smoothning with Laplacian filter
 lap = cv2.Laplacian(img, cv2.CV_64F)
-sharp = cv2.convertScaleAbs(img + lap)
-plt.figure(figsize=(8,4))
+imgL = cv2.convertScaleAbs(img+lap)
+#plotting
+plt.figure(figsize=(15,5))
 plt.subplot(1,2,1); plt.imshow(img, cmap='gray'); plt.title('Original')
-plt.subplot(1,2,2); plt.imshow(sharp, cmap='gray'); plt.title('Sharpened')
-plt.axis('off'); plt.tight_layout(); plt.show()
-# Laplacian enhances edges, making features more distinct.
+plt.subplot(1,2,2); plt.imshow(imgL, cmap='gray'); plt.title('Sharpened')
+plt.tight_layout(); plt.show()
--- 8_contrast_stretching.py
+++ 8_contrast_stretching.py
@@ -1,11 +1,15 @@
-"""Task: Contrast Stretching"""
 import cv2
 import matplotlib.pyplot as plt
+
 img = cv2.imread('image.jpg', cv2.IMREAD_GRAYSCALE)
-minV, maxV = img.min(), img.max()
+
+#contrast streching
+minV = img.min()
+maxV = img.max()
 stretched = ((img-minV)*255/(maxV-minV)).astype('uint8')
-plt.figure(figsize=(8,4))
-plt.subplot(1,2,1); plt.imshow(img, cmap='gray'); plt.title('Original')
-plt.subplot(1,2,2); plt.imshow(stretched, cmap='gray'); plt.title('Stretched')
-plt.axis('off'); plt.tight_layout(); plt.show()
-# Stretching expands pixel range, enhancing contrast.
+
+#plotting
+plt.figure(figsize=(10,4))
+plt.subplot(1,2,1); plt.imshow(img, cmap='gray');plt.title('Original')
+plt.subplot(1,2,2); plt.imshow(stretched, cmap='gray');plt.title('Streched')
+plt.tight_layout(); plt.show()
